f9fb853000a021dd88f67cff5d87c6d15c551768
Sagar Mangulkar <smangul1@binghamton.edu>
2017-04-16 03:12:12 -0400

    Implement hero up move by button

--- FunAdventure/PlayViewController.swift
+++ FunAdventure/PlayViewController.swift
@@ -9,44 +9,46 @@
 import UIKit
 
 class PlayViewController: UIViewController {
-
+    
     var location = CGPoint(x: 0, y: 0)
     
     @IBOutlet var imageHero: UIImageView!
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches{
-            imageHero.center = touch.location(in: self.view)
-        }
+    @IBOutlet var buttonUp: UIButton!
+//    
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        
+//    }
+
+    @IBAction func pushButtonUp(_ sender: Any) {
+        UIView.animate(withDuration: 1, animations: {
+            var frameHero = self.imageHero.frame
+            frameHero.origin.y = frameHero.origin.y - 30
+            self.imageHero.frame = frameHero
+        })
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches{
-            imageHero.center = touch.location(in: self.view)
-        }
-    }
-    
     
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        UIView.animate(withDuration: 3, animations: {
+            var frameHero = self.imageHero.frame
+            frameHero.origin.y = frameHero.origin.y - 10
+            self.imageHero.frame = frameHero
+        })
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
